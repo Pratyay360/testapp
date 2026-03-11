8f5941052a03847a335bd513f8b2c389abc03daa
PRATYAY MUSTAFI <pratyaymustafi@outlook.com>
2026-03-11 14:24:00 +0000

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["localhost", ".pratyay.qzz.io"],
+    allowedHosts: ["localhost", ".test.pratyay.qzz.io"],
     host: true,
     port: 5173,
   },
